9bb1c15996322ed2f222db9caf11d0960c790a94
robokae <alexanderhom19@gmail.com>
2022-02-12 20:12:08 -0800

    login and register page markup and styling

--- todo.py
+++ todo.py
@@ -1,6 +1,26 @@
+import os
 from flask import Flask, render_template, url_for, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', 
+                                validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', 
+                                validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', 
+                        validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Register')
 
 @app.route('/')
 def index():
@@ -8,8 +28,10 @@ def index():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    return render_template('register.html', form=form)
