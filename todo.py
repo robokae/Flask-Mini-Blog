f80a506e483dcbff763d0333e7ee5c2afed9b4d5
robokae <alexanderhom19@gmail.com>
2022-02-18 15:04:28 -0800

    add landing page

--- todo.py
+++ todo.py
@@ -1,37 +1,51 @@
 import os
-from flask import Flask, render_template, url_for, request, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from forms import LoginForm, RegisterForm
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', 
-                                validators=[DataRequired(), Length(min=8)])
-    submit = SubmitField('Login')
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
-class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', 
-                                validators=[DataRequired(), Length(min=8)])
-    confirm_password = PasswordField('Confirm Password', 
-                        validators=[DataRequired(), EqualTo(password)])
-    submit = SubmitField('Register')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def landing():
+    return render_template('landing.html')
 
-@app.route('/login')
+@app.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template('login.html', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        pass
+
     return render_template('register.html', form=form)
