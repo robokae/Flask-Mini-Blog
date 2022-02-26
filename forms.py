1360c16ba07d104eb36d2f6fc20444427584d73f
robokae <alexanderhom19@gmail.com>
2022-02-26 00:23:15 -0800

    implement basic user registration

--- forms.py
+++ forms.py
@@ -1,17 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', 
-                                validators=[DataRequired(), Length(min=8)])
+    password = PasswordField(
+        'Password', 
+        validators=[DataRequired(), Length(min=8)]
+    )
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', 
-                                validators=[DataRequired(), Length(min=8)])
-    confirm_password = PasswordField('Confirm Password', 
-                        validators=[DataRequired(), EqualTo(password)])
+    password = PasswordField(
+        'Password', 
+        validators=[DataRequired(), Length(min=8)]
+    )
+    confirm_password = PasswordField(
+        'Confirm Password', 
+        validators=[DataRequired(), EqualTo('password', 
+                                            'Passwords must match')])
     submit = SubmitField('Register')
+
+    # # check that the username does not already exist
+    # def validate_username(self, field):
+    #     if User.query.filter_by(username=field.data).first():
+    #         raise ValidationError('Username already exists')
--- todo.py
+++ todo.py
@@ -1,21 +1,23 @@
 import os
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_required
-from flask_bcrypt import Bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from forms import LoginForm, RegisterForm
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+                'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)          # database migrations
 login_manager = LoginManager(app)
 
 @login_manager.user_loader
@@ -28,14 +30,21 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
 
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    # hash password
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 @app.route('/')
 def landing():
     return render_template('landing.html')
-
-@app.route('/home', methods=['GET', 'POST'])
-@login_required
-def home():
-    return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,6 +56,13 @@ def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        pass
+        username = form.username.data
+        password = form.password.data
+
+        # add user to the database
+        user = User(username=username, password=password)
+
+        db.session.add(user)
+        db.session.commit()
 
     return render_template('register.html', form=form)
